9e71454020cae40560f68113a20610cf35aba54f
Alexandre-Puech <alexandre.puech3@gmail.com>
2025-07-18 17:00:26 +0200

    chore: cleaned init

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import Dropdown from "./components/Dropdown";
-
-function App() {
-  return <Dropdown label="" name="" value="" options="" onChange="" />;
-}
-
-export default App;
